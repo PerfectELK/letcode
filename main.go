6a78f4907e800db783c8bd06fc7711345fbd2346
PerfectELK <perfectelk@mail.ru>
2024-01-22 04:50:36 +0300

    feat: leetcode.com/problems/invert-binary-tree/ resolved

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/PerfectELK/letcode/tree"
 	"math"
 	"strconv"
 	"strings"
@@ -10,20 +9,156 @@ import (
 )
 
 func main() {
-	t1 := tree.SliceToTree([]int{10, 5, 15})
+	t1 := SliceToTree([]int{10, 5, 15, 50, 60, 70, 80})
+	fmt.Println(PrintTree(t1))
 	ret := invertTree(t1)
-	fmt.Println(ret)
+	fmt.Println(PrintTree(ret))
 }
 
-func invertTree(root *tree.TreeNode) *tree.TreeNode {
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	invertNode(root)
 	return root
 }
 
-func isSameTree(p *tree.TreeNode, q *tree.TreeNode) bool {
+func invertNode(
+	root *TreeNode,
+) {
+	if root == nil {
+		return
+	}
+
+	l := root.Left
+	r := root.Right
+
+	root.Left = r
+	root.Right = l
+
+	invertNode(root.Left)
+	invertNode(root.Right)
+}
+
+func getLevel(root *TreeNode) (res int) {
+	res = 1
+	queue := make([]*TreeNode, 1, 1024)
+	queue[0] = root
+
+	for {
+		hasChild := false
+		for i := 0; i < len(queue); i++ {
+			if queue[i].Left != nil || queue[i].Right != nil {
+				hasChild = true
+				break
+			}
+		}
+
+		if !hasChild {
+			break
+		}
+		res++
+
+		fQueue := queue[:len(queue)]
+		queue = queue[len(queue):]
+
+		for i := 0; i < len(fQueue); i++ {
+			if fQueue[i].Left != nil {
+				queue = append(queue, fQueue[i].Left)
+			}
+			if fQueue[i].Right != nil {
+				queue = append(queue, fQueue[i].Right)
+			}
+		}
+	}
+
+	return res
+}
+
+func PrintTree(root *TreeNode) [][]string {
+	level := getLevel(root)
+	size := 1<<uint(level) - 1
+	res := make([][]string, level)
+	for i := range res {
+		res[i] = make([]string, size)
+	}
+
+	loc := 1<<uint(level-1) - 1
+	getRes(root, 0, loc, res)
+
+	return res
+}
+
+func getRes(root *TreeNode, i, j int, res [][]string) {
+	if root == nil {
+		return
+	}
+
+	res[i][j] = strconv.Itoa(root.Val)
+
+	level := len(res)
+	if level-i-2 < 0 {
+		return
+	}
+
+	diff := 1 << uint(level-i-2)
+
+	getRes(root.Left, i+1, j-diff, res)
+	getRes(root.Right, i+1, j+diff, res)
+}
+
+func SliceToTree(arr []int) *TreeNode {
+	res := new(TreeNode)
+	res.Val = arr[0]
+	depth := 0
+	binCounter := 2
+	counter := 0
+	for _, val := range arr[1:] {
+		if counter == binCounter {
+			binCounter *= 2
+			depth++
+			counter = 0
+		}
+		fillNode(res, val, 0, depth)
+		counter++
+	}
+	return res
+}
+
+func fillNode(current *TreeNode, val int, depth int, maxDepth int) bool {
+	if depth > maxDepth {
+		return false
+	}
+	if current.Left == nil {
+		current.Left = new(TreeNode)
+		current.Left.Val = val
+		return true
+	}
+	if current.Right == nil {
+		current.Right = new(TreeNode)
+		current.Right.Val = val
+		return true
+	}
+	depth = depth + 1
+	isFilled := fillNode(current.Left, val, depth, maxDepth)
+	if isFilled {
+		return true
+	}
+	return fillNode(current.Right, val, depth, maxDepth)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
 	return isSameNodes(p, q)
 }
 
-func isSameNodes(l *tree.TreeNode, r *tree.TreeNode) bool {
+func isSameNodes(l *TreeNode, r *TreeNode) bool {
 	if l == nil && r == nil {
 		return true
 	} else if l == nil || r == nil {
@@ -833,14 +968,14 @@ func mySqrt(x int) int {
 	return lowest
 }
 
-func maxDepth(root *tree.TreeNode) int {
+func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 	return int(checkDeepNodes(root, 1))
 }
 
-func checkDeepNodes(current *tree.TreeNode, depth uint16) uint16 {
+func checkDeepNodes(current *TreeNode, depth uint16) uint16 {
 	if current.Left == nil && current.Right == nil {
 		return depth
 	}
@@ -858,12 +993,12 @@ func checkDeepNodes(current *tree.TreeNode, depth uint16) uint16 {
 	return newDepth
 }
 
-func isSymmetric(root *tree.TreeNode) bool {
+func isSymmetric(root *TreeNode) bool {
 	path := make([]bool, 0, 100)
 	return checkNodes(root, root, path)
 }
 
-func checkNodes(current *tree.TreeNode, root *tree.TreeNode, path []bool) bool {
+func checkNodes(current *TreeNode, root *TreeNode, path []bool) bool {
 	if current == nil {
 		return true
 	}
@@ -885,7 +1020,7 @@ func checkNodes(current *tree.TreeNode, root *tree.TreeNode, path []bool) bool {
 	return true
 }
 
-func checkIsNodeSymmetric(val int, root *tree.TreeNode, path []bool) bool {
+func checkIsNodeSymmetric(val int, root *TreeNode, path []bool) bool {
 	cur := root
 	for _, p := range path {
 		// reverse logic
