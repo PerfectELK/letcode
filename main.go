d9a4864b37cfe0a17f3e03f67c474c45f2e17718
PerfectELK <perfectelk@mail.ru>
2024-04-27 20:21:19 +0300

    feat: https://leetcode.com/problems/single-number/ resolved

--- main.go
+++ main.go
@@ -16,6 +16,18 @@ func main() {
 }
 
 func singleNumber(nums []int) int {
+	m := make(map[int]int, len(nums)/2+1)
+
+	for _, v := range nums {
+		m[v]++
+	}
+
+	for k, v := range m {
+		if v == 1 {
+			return k
+		}
+	}
+
 	return 0
 }
 
