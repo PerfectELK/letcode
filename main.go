88abd86ae7c3f7ec7593c172b86e7ffaa234447c
PerfectELK <perfectelk@mail.ru>
2024-05-22 01:18:25 +0300

    feat: https://leetcode.com/problems/contains-duplicate resolved

--- main.go
+++ main.go
@@ -11,11 +11,22 @@ import (
 
 func main() {
 
-	arr := []int{7, 1, 5, 3, 6, 4}
-	//arr := []int{7, 6, 4, 3, 1}
+	arr := []int{1, 2, 3, 4, 5, 1}
 
-	r := maxProfit(arr)
+	r := containsDuplicate(arr)
 	fmt.Println(r)
+}
+
+func containsDuplicate(nums []int) bool {
+	c := make(map[int]struct{}, len(nums))
+
+	for _, n := range nums {
+		if _, ok := c[n]; ok {
+			return true
+		}
+		c[n] = struct{}{}
+	}
+	return false
 }
 
 func maxProfit(prices []int) int {
