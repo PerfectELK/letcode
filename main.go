24bdf935363aa4c7587e9871eff8043df61e8dba
PerfectELK <perfectelk@mail.ru>
2025-01-24 01:04:09 +0300

    leetcode.com/problems/add-digits init

--- main.go
+++ main.go
@@ -12,23 +12,28 @@ import (
 )
 
 func main() {
-	lRUCache := Constructor(2)
-	// lRUCache.Put(1, 1) // cache is {1=1}
-	// lRUCache.Put(2, 2) // cache is {1=1, 2=2}
-	// lRUCache.Get(1)    // return 1
-	// lRUCache.Put(3, 3) // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-	// lRUCache.Get(2)    // returns -1 (not found)
-	// lRUCache.Put(4, 4) // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-	// lRUCache.Get(1)    // return -1 (not found)
-	// lRUCache.Get(3)    // return 3
-	// lRUCache.Get(4)    // return 4
 
-	lRUCache.Put(2, 1)
-	lRUCache.Put(2, 2)
-	fmt.Println(lRUCache.Get(2))
-	lRUCache.Put(1, 1)
-	lRUCache.Put(4, 1)
-	fmt.Println(lRUCache.Get(2))
+	fmt.Println(100000 / 10)
+
+}
+
+func addDigits(num int) int {
+	if num < 10 {
+		return num
+	}
+
+	for num >= 10 {
+
+	}
+
+	return 0
+}
+
+func digestOfNum(num int) []int {
+	if num < 10 {
+		return []int{num}
+	}
+	return nil
 }
 
 type LRUCache struct {
