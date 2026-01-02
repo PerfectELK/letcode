9f3600c51a88d1185d49d836a29011f46ccfc0ff
PerfectELK <perfectelk@mail.ru>
2026-01-02 13:58:01 +0300

    rust

---
+++ letcode/src/main.rs
@@ -0,0 +1,28 @@
+mod tests;
+
+struct Solution;
+impl Solution {
+    pub fn convert_to_title(column_number: i32) -> String {
+        if column_number <= 0 {
+            return "".to_string();
+        }
+
+        let mut num = column_number;
+        let mut builder = String::new();
+        while num > 0 {
+            num -= 1;
+            let remainder = (num % 26) as u8;
+            let ch = (b'A' + remainder) as char;
+            builder.insert(0, ch);
+            num /= 26;
+        }
+        builder
+    }
+}
+
+
+fn main() {
+    let r = Solution::convert_to_title(12);
+    println!("{}", r);
+}
+
---
+++ letcode/src/tests.rs
@@ -0,0 +1,27 @@
+use crate::Solution;
+
+#[test]
+fn convert_to_title(){
+    struct TestCase {
+        column_number: i32,
+        result: String,
+    }
+
+    let cases = vec![
+        // TestCase{column_number: 0, result: "".to_string()},
+        // TestCase{column_number: 1, result: "A".to_string()},
+        //TestCase{column_number: 26, result: "Z".to_string()},
+        TestCase{column_number: 52, result: "AZ".to_string()},
+        // TestCase{column_number: 28, result: "AB".to_string()},
+        // TestCase{column_number: 701, result: "ZY".to_string()},
+        // TestCase{column_number: 703, result: "AAA".to_string()},
+    ];
+
+    for test_case in cases {
+        let result = Solution::convert_to_title(test_case.column_number);
+        assert_eq!(
+            test_case.result, result,
+            "Input {}", test_case.column_number
+        );
+    }
+}
